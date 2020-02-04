7b7e0826d8094e092f3abb8d88cca05422092e5e
sayersmx <sayersmx@mail.uc.edu>
2020-02-04 18:32:10 -0500

    adding powershell files

---
+++ powershell/lab3.ps1
@@ -0,0 +1,26 @@
+﻿#This is a script to send an email with information about our machine.
+
+#A funtion to return the IP address
+function getIP {(get-netipaddress).ipv4address | select-string "192*"}
+
+
+#Set the IP variable
+$IP = getIP
+
+#Set the date variable
+$date = Get-Date -DisplayHint Date
+
+#Set the name variable
+$name = $env:computername
+
+#Set the operating system variable
+$os = $env:os
+
+#Set the body variable
+$BODY = "This machine's IP is $IP, the date is $date, the name of the machine is $name, and the operating system is $os"
+
+#Build and send the email
+Send-MailMessage -To "sayersmx@mail.uc.edu" -From "me@mail.uc.edu" -Subject "IT3038C SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSsl -Credential (Get-Credential)
+
+#Confirm the email was sent
+Write-Host("Message Sent")
---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,4 @@
+﻿Function getIP {(Get-NetIPAddress).IPv4Address | select-string "192*"}
+$IP = getIP
+Write-Host("This machine's IP is $IP")
+Write-Host("This machine's IP is {0}" -f $IP)
